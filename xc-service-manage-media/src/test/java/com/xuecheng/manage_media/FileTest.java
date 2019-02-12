beda9d11cbf2dfa58b97cbda6a07ed84175b3016
zs <483828079@qq.com>
2019-02-12 23:19:46 +0800

    19 0212 2319

---
+++ xc-service-manage-media/src/test/java/com/xuecheng/manage_media/FileTest.java
@@ -0,0 +1,16 @@
+package com.xuecheng.manage_media;
+
+import org.junit.*;
+
+public class FileTest {
+
+	/**
+	 * 文件分块测试
+	 * 读取文件将该文件分块存储
+	 * @throws Exception
+	 */
+	@Test
+	public void testChunk() throws Exception{
+	    
+	}
+}
---
+++ xc-service-manage-media/src/test/java/com/xuecheng/manage_media/RandomAccessFileTest.java
@@ -0,0 +1,14 @@
+package com.xuecheng.manage_media;
+import org.junit.*;
+
+/**
+ * RandomAccessFile 的测试。
+ *
+ */
+public class RandomAccessFileTest {
+
+	@Test
+	public void testWrite() throws Exception{
+
+	}
+}
