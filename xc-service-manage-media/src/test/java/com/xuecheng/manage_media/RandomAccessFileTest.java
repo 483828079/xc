1acde19b934962e8f9e084d585d2cb733cd8642b
zs <483828079@qq.com>
2019-02-12 23:31:26 +0800

    19 0212 2331

--- xc-service-manage-media/src/test/java/com/xuecheng/manage_media/RandomAccessFileTest.java
+++ xc-service-manage-media/src/test/java/com/xuecheng/manage_media/RandomAccessFileTest.java
@@ -1,6 +1,8 @@
 package com.xuecheng.manage_media;
 import org.junit.*;
 
+import java.io.RandomAccessFile;
+
 /**
  * RandomAccessFile 的测试。
  *
@@ -9,6 +11,7 @@ public class RandomAccessFileTest {
 
 	@Test
 	public void testWrite() throws Exception{
-
+		//
+		RandomAccessFile randomAccessFile = new RandomAccessFile();
 	}
 }
